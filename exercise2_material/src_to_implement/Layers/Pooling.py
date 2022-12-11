bd0ce86405fed76700bda850e7893ac3422a9c78
Anugya Sahu <92376737+AnugyaSahu@users.noreply.github.com>
2022-12-11 21:39:57 +0100

    optimized pooling layer

--- exercise2_material/src_to_implement/Layers/Pooling.py
+++ exercise2_material/src_to_implement/Layers/Pooling.py
@@ -13,20 +13,27 @@ class Pooling(Base.BaseLayer):
     def forward(self, input_tensor):
         self.input_tensor = input_tensor
         self.output_tensor = np.zeros((input_tensor.shape[0], input_tensor.shape[1], (input_tensor.shape[2]-self.pooling_shape[0])//self.stride_shape[0] + 1, (input_tensor.shape[3]-self.pooling_shape[1])//self.stride_shape[1] + 1))
+        self.max_pos = np.zeros(self.output_tensor.shape)
         for i in range(input_tensor.shape[0]):
             for j in range(input_tensor.shape[1]):
                 for pi in range(self.output_tensor.shape[2]):
                     for pj in range(self.output_tensor.shape[3]):
-                        self.output_tensor[i, j,pi ,pj ] = np.max(input_tensor[i, j, self.stride_shape[0]*pi:pi*self.stride_shape[0]+self.pooling_shape[0], self.stride_shape[1]*pj:pj*self.stride_shape[1]+self.pooling_shape[1]])
-        self.output_tensor = self.output_tensor.astype(int)
+                        a = self.stride_shape[0]*pi
+                        b = self.stride_shape[1]*pj
+                        self.output_tensor[i, j,pi ,pj] = np.max(input_tensor[i, j, a:a+self.pooling_shape[0], b:b+self.pooling_shape[1]])
+                        self.max_pos[i, j, pi, pj] = np.argmax(input_tensor[i, j, a:a+self.pooling_shape[0], b:b+self.pooling_shape[1]])
+        self.max_pos = self.max_pos.astype(int)
         return self.output_tensor
     
     def backward(self, error_tensor):
-        self.max_pos = np.zeros(self.input_tensor.shape)
+        error = np.zeros(self.input_tensor.shape)
         for i in range(self.input_tensor.shape[0]):
             for j in range(self.input_tensor.shape[1]):
                 for pi in range(error_tensor.shape[2]):
                     for pj in range(error_tensor.shape[3]):
-                        self.max_pos[i, j, self.stride_shape[0]*pi:pi*self.stride_shape[0]+self.pooling_shape[0], self.stride_shape[1]*pj:pj*self.stride_shape[1]+self.pooling_shape[1]] = np.where(np.max(self.input_tensor[i, j, self.stride_shape[0]*pi:pi*self.stride_shape[0]+self.pooling_shape[0], self.stride_shape[1]*pj:pj*self.stride_shape[1]+self.pooling_shape[1]])==self.input_tensor[i, j, self.stride_shape[0]*pi:pi*self.stride_shape[0]+self.pooling_shape[0], self.stride_shape[1]*pj:pj*self.stride_shape[1]+self.pooling_shape[1]], self.input_tensor[i, j, self.stride_shape[0]*pi:pi*self.stride_shape[0]+self.pooling_shape[0], self.stride_shape[1]*pj:pj*self.stride_shape[1]+self.pooling_shape[1]], 0)
-        self.max_pos = self.max_pos.astype(int)
-        return self.max_pos
+                        a, b = np.unravel_index(self.max_pos[i, j,pi,pj], (self.pooling_shape[0], self.pooling_shape[1]))        
+                        if error[i, j, a+self.stride_shape[0]*pi, b+self.stride_shape[1]*pj] != 0.0:
+                            error[i, j,a+self.stride_shape[0]*pi, b+self.stride_shape[1]*pj] += error_tensor[i, j, pi, pj]
+                        else:
+                            error[i, j,a+self.stride_shape[0]*pi, b+self.stride_shape[1]*pj] = error_tensor[i, j, pi, pj]
+        return error
