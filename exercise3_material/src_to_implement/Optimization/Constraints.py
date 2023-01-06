4a22cc2fd9f9c8ef0a7b9508f56a64bedfc1d673
Anugya Sahu <92376737+AnugyaSahu@users.noreply.github.com>
2023-01-06 09:02:54 +0100

    Update Constraints.py

--- exercise3_material/src_to_implement/Optimization/Constraints.py
+++ exercise3_material/src_to_implement/Optimization/Constraints.py
@@ -1,23 +1,21 @@
 import numpy as np
 
-class L2_Regularizer:
-
-    def __init__(self,alpha):
-        self.alpha=alpha
-    
-    def calculate_gradient(weights):
-        pass
-
-    def norm(weights):
-        pass
-
 class L1_Regularizer:
+    def __init__(self, alpha):
+        self.alpha = alpha
 
-    def __init__(self,alpha):
-        self.alpha=alpha
+    def calculate_gradient(self, weights):
+        return self.alpha * np.sign(weights)
+
+    def norm(self, weights):
+        return self.alpha * np.abs(weights).sum()
     
-    def calculate_gradient(weights):
-        pass
+class L2_Regularizer:
+    def __init__(self, alpha):
+        self.alpha = alpha
 
-    def norm(weights):
-        pass
+    def calculate_gradient(self, weights):
+        return self.alpha * weights
+
+    def norm(self, weights):
+        return self.alpha * (weights**2).sum()
