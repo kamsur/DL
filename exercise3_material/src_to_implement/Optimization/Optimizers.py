819e7e117c432b641724a1cdb26f212986423ac0
Anugya Sahu <92376737+AnugyaSahu@users.noreply.github.com>
2023-01-06 09:00:04 +0100

    Update Optimizers.py

--- exercise3_material/src_to_implement/Optimization/Optimizers.py
+++ exercise3_material/src_to_implement/Optimization/Optimizers.py
@@ -3,33 +3,44 @@ import numpy as np
 class Optimizer:
     def __init__(self):
         self.regularizer = None
-    def add_regularizer(self, regularizer):
-        self.regularizer=regularizer
-    
+
+    def add_regularizer(self, value):
+        self.regularizer = value
+        
 class Sgd(Optimizer):
     def __init__(self, learning_rate):
-        super(Optimizer, self).__init__()
+        super(Sgd, self).__init__()
         self.learning_rate = learning_rate
     
     def calculate_update(self, weight_tensor, gradient_tensor):
+        
+        if self.regularizer:
+            reg = self.regularizer.calculate_gradient(weight_tensor)
+            weight_tensor = weight_tensor - self.learning_rate * reg
+            
         weight_tensor1 = weight_tensor - self.learning_rate*gradient_tensor
         return weight_tensor1
     
 class SgdWithMomentum(Optimizer):
     def __init__(self, learning_rate, momentum_rate):
-        super(Optimizer, self).__init__()
+        super(SgdWithMomentum, self).__init__()
         self.learning_rate = learning_rate
         self.momentum_rate = momentum_rate
         self.v_k = 0
         
     def calculate_update(self, weight_tensor, gradient_tensor):
+        
+        if self.regularizer:
+            reg = self.regularizer.calculate_gradient(weight_tensor)
+            weight_tensor = weight_tensor - self.learning_rate * reg
+            
         self.v_k = self.momentum_rate*self.v_k - self.learning_rate*gradient_tensor
         weight_tensor1 = weight_tensor + self.v_k
         return weight_tensor1
 
 class Adam(Optimizer):
     def __init__(self, learning_rate, mu, rho):
-        super(Optimizer, self).__init__()
+        super(Adam, self).__init__()
         self.learning_rate = learning_rate
         self.mu, self.rho = mu, rho
         self.v_k, self.r_k = 0, 0
@@ -37,6 +48,11 @@ class Adam(Optimizer):
         self.k = 1
     
     def calculate_update(self, weight_tensor, gradient_tensor):
+        
+        if self.regularizer:
+            reg = self.regularizer.calculate_gradient(weight_tensor)
+            weight_tensor = weight_tensor - self.learning_rate * reg
+            
         self.v_k = self.mu*self.v_k + (1 - self.mu)*gradient_tensor
         self.r_k = self.rho*self.r_k + (1 - self.rho)*gradient_tensor*gradient_tensor
         self.v_hat_k = self.v_k/(1 - self.mu**self.k)
@@ -44,3 +60,4 @@ class Adam(Optimizer):
         self.k += 1
         weight_tensor1 = weight_tensor - self.learning_rate*(self.v_hat_k/(np.sqrt(self.r_hat_k)+np.finfo(float).eps))
         return weight_tensor1
+
