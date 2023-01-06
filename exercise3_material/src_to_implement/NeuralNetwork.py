5da2ee25a277eeb18ae02afdb4c16088f0abd229
Anugya Sahu <92376737+AnugyaSahu@users.noreply.github.com>
2023-01-06 09:04:29 +0100

    Update NeuralNetwork.py

--- exercise3_material/src_to_implement/NeuralNetwork.py
+++ exercise3_material/src_to_implement/NeuralNetwork.py
@@ -10,14 +10,6 @@ class NeuralNetwork:
         self.layers = []
         self.data_layer = []
         self.loss_layer = []
-
-    @property
-    def phase(self):
-        return self._phase 
-    
-    @phase.setter
-    def phase(self, value):
-        self._phase = value
     
     def forward(self, input_tensor=None, label_tensor=None):
         if input_tensor is None and label_tensor is None:
@@ -25,9 +17,18 @@ class NeuralNetwork:
         else:
             self.input_tensor,self.label_tensor = input_tensor,label_tensor
         prediction_tensor=self.input_tensor
+        regularization_loss = 0
         for layer in self.layers:
             prediction_tensor=layer.forward(prediction_tensor)
-        return self.loss_layer.forward(prediction_tensor,self.label_tensor) 
+            try:
+                if layer.optimizer:
+                        if layer.optimizer.regularizer:
+                            regularization_loss += layer.optimizer.regularizer.norm(layer.weights)
+            except(AttributeError):
+                pass
+        loss = self.loss_layer.forward(prediction_tensor,self.label_tensor)
+        self.loss.append(loss+regularization_loss)
+        return loss + regularization_loss
 
     def backward(self, label_tensor=None):
         if label_tensor is None:
@@ -45,14 +46,17 @@ class NeuralNetwork:
         self.layers.append(layer)
     
     def train(self, iterations):
+        self.testing_phase = False
         for i in range(iterations):
             input_tensor,label_tensor = self.data_layer.next()
             self.loss.append(self.forward(input_tensor,label_tensor))
             self.backward(label_tensor)
             
     def test(self, input_tensor):
+        self.testing_phase = True
         prediction_tensor=input_tensor
         for layer in self.layers:
+            layer.testing_phase = True
             prediction_tensor=layer.forward(prediction_tensor)
         return prediction_tensor
             
