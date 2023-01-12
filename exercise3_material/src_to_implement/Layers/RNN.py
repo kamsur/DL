eb4969deba65eca198708ee853c4ed884c1db0bd
kamsur <57803389+kamsur@users.noreply.github.com>
2023-01-12 23:52:12 +0100

    RNN update 3

--- exercise3_material/src_to_implement/Layers/RNN.py
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -18,9 +18,9 @@ class RNN(Base.BaseLayer):
         self.h_t=None
         self.last_seq_h=None
         self.time_size=None
-        self.input_tensor = None
+        self.input_tensor_h = None
+        self.input_tensor_y = None
         self.FC_h = FullyConnected.FullyConnected(hidden_size + input_size, hidden_size)
-
         self.tanH=TanH.TanH()
         self.FC_y = FullyConnected.FullyConnected(hidden_size, output_size)
         
@@ -60,12 +60,12 @@ class RNN(Base.BaseLayer):
         self.FC_y.initialize(weights_initializer, bias_initializer)
         self.FC_h.initialize(weights_initializer, bias_initializer)
         self.weights = self.FC_h.weights
-        self.gradient_weights = self.FC_h.gradient_weights
-
+        self.gradient_weights=self.FC_h.gradient_weights
     
     def forward(self, input_tensor):
-        self.input_tensor = input_tensor
         self.time_size=input_tensor.shape[0]
+        self.input_tensor_h = np.empty( self.time_size, dtype=object)
+        self.input_tensor_y = np.empty( self.time_size, dtype=object)
         if self.memorize:
             if self.h_t is None:
                 self.h_t = np.zeros((self.time_size + 1, self.hidden_size))
@@ -80,7 +80,10 @@ class RNN(Base.BaseLayer):
             x = input_tensor[t][np.newaxis, :]
             xtilda_t = np.concatenate((h, x), axis = 1)
             self.h_t[t+1] = self.tanH.forward(self.FC_h.forward(xtilda_t))
+            self.input_tensor_h[t]=self.FC_h.input_tensor
             y_t[t] = (self.FC_y.forward(self.h_t[t + 1][np.newaxis, :]))
+            self.input_tensor_y[t]=self.FC_y.input_tensor
+
         
         self.last_seq_h = self.h_t[-1]
 
@@ -89,25 +92,47 @@ class RNN(Base.BaseLayer):
     def backward(self, error_tensor):
         self.Ex_out=np.zeros((self.time_size, self.input_size))
         Eh = np.zeros((1, self.hidden_size))
+        gradient_weights_y=None
+        gradient_weights_h=None
         for t in reversed(range(self.time_size)):
-            self.FC_y.input_tensor = np.hstack((self.h_t[t+1], 1))[np.newaxis, :]
+            self.FC_y.input_tensor = self.input_tensor_y[t]
+            #print("Y input=",self.FC_y.input_tensor)
             Ey = self.FC_y.backward(error_tensor[t][np.newaxis, :])
+            if gradient_weights_y is not None:
+                gradient_weights_y = gradient_weights_y+self.FC_y.gradient_weights
+            else:
+                gradient_weights_y = self.FC_y.gradient_weights
             grad_yh = Eh + Ey
             self.tanH.tanh=self.h_t[t+1]
             grad_h = self.tanH.backward(grad_yh)
-            self.FC_h.input_tensor = np.hstack((self.h_t[t], self.input_tensor[t],1))[np.newaxis, :]
+            self.FC_h.input_tensor = self.input_tensor_h[t]
+            #print("H input=",self.FC_h.input_tensor)
             Exh = self.FC_h.backward(grad_h)
+            #print("Grad_weight_h=",self.FC_h.gradient_weights)
+            if gradient_weights_h is not None:
+                gradient_weights_h = gradient_weights_h+self.FC_h.gradient_weights
+            else:
+                gradient_weights_h = self.FC_h.gradient_weights
+            #print("Grad_weight_h=",gradient_weights_h)
             Eh = Exh[:, 0:self.hidden_size]
             Ex = Exh[:, self.hidden_size:(self.hidden_size + self.input_size + 1)]
             self.Ex_out[t] = Ex
-            self.weights = self.FC_h.weights
-            self.gradient_weights = self.FC_h.gradient_weights
+        self.gradient_weights=gradient_weights_h
+        #print("Grad_weight=",self.gradient_weights)
 
         if self.optimizer is not None:
-            weights_y = self.optimizer.calculate_update(self.FC_y.weights, self.FC_y.gradient_weights)
+            weights_y = self.optimizer.calculate_update(self.FC_y.weights, gradient_weights_y)
             self.weights = self.optimizer.calculate_update(self.weights, self.gradient_weights)
             self.FC_y.weights = weights_y
             self.FC_h.weights = self.weights
         return self.Ex_out
+
+    def calculate_regularization_loss(self):
+        reg_loss = 0
+        if self.optimizer:
+            if self.optimizer.regularizer:
+                reg_loss += self.optimizer.regularizer.norm(self.weights)
+        return reg_loss
+
 
 
