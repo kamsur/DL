528a3efcc12d225c6e13a3a9c05e91be8f9e3f0b
kamsur <57803389+kamsur@users.noreply.github.com>
2023-01-26 22:34:55 +0100

    Update data.py

--- exercise4_material/src_to_implement/data.py
+++ exercise4_material/src_to_implement/data.py
@@ -5,11 +5,22 @@ from skimage.io import imread
 from skimage.color import gray2rgb
 import numpy as np
 import torchvision as tv
-
-train_mean = [0.59685254, 0.59685254, 0.59685254]
-train_std = [0.16043035, 0.16043035, 0.16043035]
+from pandas import DataFrame
 
 
 class ChallengeDataset(Dataset):
+    def __init__(self, data:DataFrame, mode:str):
+        super().__init__()
+        self.data = data
+        self.mode = mode
+        self._transform = tv.transforms.Compose()
     # TODO implement the Dataset class according to the description
-    pass
+    #pass
+
+    @property
+    def transform(self):
+        return self._transform
+
+    @transform.setter
+    def transform(self, transforms_list):
+        self._transform = tv.transforms.Compose(transforms_list)
